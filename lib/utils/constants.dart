04c8d5863e3bae4b0fa12549bc0d527d9e55189d
avi221 <35756548+avi221@users.noreply.github.com>
2020-06-20 21:28:41 +0530

    full demo

--- lib/utils/constants.dart
+++
@@ -1,9 +0,0 @@
-class StringLiteral {
-  static String allOrders = "All\nOrders";
-  static String earningDay = "Earnings of\nthe day";
-  static String allCustomers = "All Customers";
-  static String ongoing = "Ongoing";
-  static String tom = "Tomorow";
-  static String history = "History";
-  static String instant = "Instant";
-}
